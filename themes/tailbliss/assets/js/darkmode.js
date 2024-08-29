c5bcc8de6c13b95a856f0dcef84ef842bb491f97
Dave Borgeest <david.borgeest@gmail.com>
2024-08-29 19:35:07 +1000

    light theme only

--- themes/tailbliss/assets/js/darkmode.js
+++ themes/tailbliss/assets/js/darkmode.js
@@ -2,38 +2,11 @@ var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Always set the light theme by default
-themeToggleDarkIcon.classList.remove('hidden');
-themeToggleLightIcon.classList.add('hidden');
+themeToggleDarkIcon.classList.add('hidden');
+themeToggleLightIcon.classList.remove('hidden');
 document.documentElement.classList.remove('dark');
 localStorage.setItem('color-theme', 'light');
 
+// Optionally, you can disable the theme toggle button to prevent user interaction
 var themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
-});
+themeToggleBtn.disabled = true;
