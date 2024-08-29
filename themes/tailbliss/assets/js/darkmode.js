ad5cf13642071fa99682e9aabf931526cbdb4b4b
Dave Borgeest <david.borgeest@gmail.com>
2024-08-29 19:23:34 +1000

    default light theme

--- themes/tailbliss/assets/js/darkmode.js
+++ themes/tailbliss/assets/js/darkmode.js
@@ -1,12 +1,11 @@
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
-} else {
-    themeToggleDarkIcon.classList.remove('hidden');
-}
+// Always set the light theme by default
+themeToggleDarkIcon.classList.remove('hidden');
+themeToggleLightIcon.classList.add('hidden');
+document.documentElement.classList.remove('dark');
+localStorage.setItem('color-theme', 'light');
 
 var themeToggleBtn = document.getElementById('theme-toggle');
 
