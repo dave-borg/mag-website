adb24d39a6203f06d908f1029a1dc07cd6862d65
Dave Borgeest <david.borgeest@gmail.com>
2024-08-29 19:42:38 +1000

    simple js

--- themes/tailbliss/assets/js/darkmode.js
+++ themes/tailbliss/assets/js/darkmode.js
@@ -1,12 +1,12 @@
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+// var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+// var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-// Always set the light theme by default
-themeToggleDarkIcon.classList.add('hidden');
-themeToggleLightIcon.classList.remove('hidden');
+// // Always set the light theme by default
+// themeToggleDarkIcon.classList.add('hidden');
+// themeToggleLightIcon.classList.remove('hidden');
 document.documentElement.classList.remove('dark');
 localStorage.setItem('color-theme', 'light');
 
 // Optionally, you can disable the theme toggle button to prevent user interaction
-var themeToggleBtn = document.getElementById('theme-toggle');
-themeToggleBtn.disabled = true;
+// var themeToggleBtn = document.getElementById('theme-toggle');
+// themeToggleBtn.disabled = true;
